bb5bcc71b608bc528864a8f8fae6783e13be9df7
Amir Kasumovic <amir.kasumovic@live.com>
2021-11-02 16:04:25 +0100

    mobile testimonial

--- src/Pages/Mobile/HomeMobile/HomeMobile.tsx
+++ src/Pages/Mobile/HomeMobile/HomeMobile.tsx
@@ -238,6 +238,20 @@ export const HomeMobile = (props: Props) => {
             </div>
           </div>
         </div>
+        <div className='testimonial'>
+          <h1 className='testimonialTitle'>TESTIMONIAL</h1>
+          <a
+            className='testimonialContent'
+            href='https://ideaology.io'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <img
+              src='./ideaologyLogo.svg'
+              alt='Ideaology Logo'
+              className='ideaologyLogo'
+            />
+          </a>
+        </div>
         <Footer />
       </div>
     </div>
